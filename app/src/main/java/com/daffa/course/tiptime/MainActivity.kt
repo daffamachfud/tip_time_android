468fda479166957aa7d30c91815705c360a48472
Daffa Radifanka <daffa.radifanka@dretail.id>
2023-11-21 10:58:43 +0700

    finish codelab

--- app/src/main/java/com/daffa/course/tiptime/MainActivity.kt
+++ app/src/main/java/com/daffa/course/tiptime/MainActivity.kt
@@ -2,10 +2,45 @@ package com.daffa.course.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.daffa.course.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_eighteen_percent -> 0.18
+            R.id.option_twenty_percent -> 0.20
+            else -> 0.15
+        }
+
+        val tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
     }
 }
